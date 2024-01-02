e1ba94572513047629e9ca7eb4f6aae972629e19
hvhien <vanhienfs@gmail.com>
2024-01-02 15:16:34 +0700

    javascript noted with datatype

---
+++ javascript-notes/dataType.js
@@ -0,0 +1,16 @@
+// javascipt has dynamic type, that mean, variable can hole all of data type
+// have 2 kind of data type
+// 1. primitive types: string, number, boolean, null, undefined
+// 2. complex types: object,function
+
+function dataType(){
+    let v1="hello word"; // string
+    let v2=10;           // number
+    let v3=true;        // boolean
+    let v4=null;        //null
+    let v5=undefined;   //undefined
+    //complex types
+    let obj={fistname:"hien",lastname:"ha",job:"Software Enginee",salary:'$2000'}; // object
+    let printObject=(obj)=>{console.log(obj);} // function
+    functionVar(obj);
+}
